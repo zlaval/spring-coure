72b07899e8ed58924943980a1efb5a2469549081
zlava <zlava@gmail.com>
2025-08-13 18:13:49 +0200

    feat: 5 - error handling v3

--- src/main/java/hu/zlaval/springcourse/configuration/GlobalErrorHandler.java
+++ src/main/java/hu/zlaval/springcourse/configuration/GlobalErrorHandler.java
@@ -2,13 +2,16 @@ package hu.zlaval.springcourse.configuration;
 
 
 import hu.zlaval.springcourse.configuration.exceptions.NotFoundException;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.net.URI;
+import java.util.AbstractMap;
 
 //@ControllerAdvice
 @RestControllerAdvice
@@ -31,4 +34,32 @@ public class GlobalErrorHandler {
         pd.setType(URI.create("/spring-course"));
         return ResponseEntity.internalServerError().body(pd);
     }
+
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ProblemDetail> handleValidationError(BindException error) {
+        var problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        problemDetail.setTitle("Validation error");
+        problemDetail.setDetail("Fields are invalid");
+        problemDetail.setType(URI.create("/spring-course"));
+        var errors = error.getBindingResult().getAllErrors()
+                .stream().map(entry -> {
+                            var message = entry.getDefaultMessage();
+                            String property = entry.getCode();
+                            if (entry.contains(ConstraintViolation.class)) {
+                                var cv = entry.unwrap(ConstraintViolation.class);
+                                var f = cv.getPropertyPath().toString();
+                                if (!f.isBlank()) {
+                                    property = f;
+                                }
+                            }
+                            return new AbstractMap.SimpleEntry<>(property, message);
+                        }
+                );
+
+        problemDetail.setProperty("invalid_fields", errors);
+
+        return ResponseEntity.badRequest().body(problemDetail);
+    }
+
 }
